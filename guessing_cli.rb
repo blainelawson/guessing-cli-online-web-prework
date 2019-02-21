409053f34cce3533a6e6b2cf429517da98399fce
Blaine Lawson <blaine.lawson@gmail.com>
2019-02-21 23:21:12 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -4,7 +4,7 @@ def run_guessing_game
   guess = nil
   
   
-      puts "Guess a number between 1 and 6."
+    puts "Guess a number between 1 and 6."
     guess = gets.chomp
   
     if guess.to_i == rand_num
